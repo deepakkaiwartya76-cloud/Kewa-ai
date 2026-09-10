0f8496817f866c0279d0fb5f904b1e886cbdb8ac
deepakkaiwartya76-cloud <deepakkaiwartya76@gmail.com>
2026-09-10 12:56:02 +0530

    Update firebase.js

--- Backend/config/firebase.js
+++ Backend/config/firebase.js
@@ -1,11 +1,10 @@
 const admin = require('firebase-admin');
 
-// Initialize Firebase Admin with direct project ID to bypass env issue
 admin.initializeApp({
   credential: admin.credential.cert({
     projectId: "kewa-webs-fefe0",
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n') : undefined
+    clientEmail: "yahan_apna_client_email_daal_firebase_json_se",
+    privateKey: "-----BEGIN PRIVATE KEY-----\nyahan_apni_private_key_daal_bina_quotes_ke\n-----END PRIVATE KEY-----".replace(/\\n/g, '\n')
   })
 });
 
