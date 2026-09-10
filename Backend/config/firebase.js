540dc50f831a3f8f6181d83e9a4f8a69f28328c0
deepakkaiwartya76-cloud <deepakkaiwartya76@gmail.com>
2026-09-10 13:04:05 +0530

    Update firebase.js

--- Backend/config/firebase.js
+++ Backend/config/firebase.js
@@ -1,10 +1,15 @@
 const admin = require('firebase-admin');
 
+// Safely reading from environment variables
+const privateKey = process.env.FIREBASE_PRIVATE_KEY 
+  ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n') 
+  : undefined;
+
 admin.initializeApp({
   credential: admin.credential.cert({
     projectId: "kewa-webs-fefe0",
-    clientEmail: "firebase-adminsdk-fbsvc@kewa-webs-fefe0.iam.gserviceaccount.com",
-    privateKey: "-----BEGIN PRIVATE KEY-----\nyahan_apni_private_key_daal_bina_quotes_ke\n-----END PRIVATE KEY-----".replace(/\\n/g, '\n')
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: privateKey
   })
 });
 
