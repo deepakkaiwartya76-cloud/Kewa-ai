942ddb9d90954d159f18f22dc85839f0941e6586
deepakkaiwartya76-cloud <deepakkaiwartya76@gmail.com>
2026-09-10 13:02:10 +0530

    Update firebase.js

--- Backend/config/firebase.js
+++ Backend/config/firebase.js
@@ -3,7 +3,7 @@ const admin = require('firebase-admin');
 admin.initializeApp({
   credential: admin.credential.cert({
     projectId: "kewa-webs-fefe0",
-    clientEmail: "yahan_apna_client_email_daal_firebase_json_se",
+    clientEmail: "firebase-adminsdk-fbsvc@kewa-webs-fefe0.iam.gserviceaccount.com",
     privateKey: "-----BEGIN PRIVATE KEY-----\nyahan_apni_private_key_daal_bina_quotes_ke\n-----END PRIVATE KEY-----".replace(/\\n/g, '\n')
   })
 });
