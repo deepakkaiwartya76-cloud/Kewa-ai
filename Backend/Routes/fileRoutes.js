d9929f145e0664afe356e26c23fd9027340473e4
deepakkaiwartya76-cloud <deepakkaiwartya76@gmail.com>
2026-09-10 10:29:31 +0530

    Delete Backend/Routes directory

--- Backend/Routes/fileRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const upload = require('../middleware/uploadMiddleware');
-const { handleFileUpload } = require('../controllers/fileController');
-
-router.post('/', upload.single('file'), handleFileUpload);
-
-module.exports = router;
--- Backend/Routes/imageRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { handleImageGeneration } = require('../controllers/imageController');
-
-router.post('/', handleImageGeneration);
-
-module.exports = router;
