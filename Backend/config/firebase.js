183894bf53399117f3fabcc45f135a13854210d0
deepakkaiwartya76-cloud <deepakkaiwartya76@gmail.com>
2026-09-10 12:41:41 +0530

    Update firebase.js

--- Backend/config/firebase.js
+++ Backend/config/firebase.js
@@ -1,14 +1,12 @@
 const admin = require('firebase-admin');
-const { firebase } = require('./env');
 
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: firebase.projectId,
-      clientEmail: firebase.clientEmail,
-      privateKey: firebase.privateKey ? firebase.privateKey.replace(/\\n/g, '\n') : undefined,
-    }),
-  });
-}
+// Initialize Firebase Admin with Environment Variables
+admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n') : undefined
+  })
+});
 
 module.exports = admin;
